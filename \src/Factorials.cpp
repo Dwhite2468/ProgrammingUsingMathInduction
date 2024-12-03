0740a79a5596281dc7e4e6bcbe848f0f382a4559
Dwhite2468 <urkman2468@gmail.com>
2024-12-02 20:20:50 -0500

    Update Factorials.cpp

--- \src/Factorials.cpp
+++ \src/Factorials.cpp
@@ -2,6 +2,23 @@
 
 using namespace std;
 
+int factorial(int factToFind)
+{
+    /**
+     *  Base case: 0! = 1
+     */
+    if(factToFind == 0)
+    {
+         return 1;
+    }
+    /**
+     * Inductive Assumption:  factorial(n - 1) is a decidable function.
+     * Inductive Step: n! = n * factorial(n - 1) is an O(1) modification
+     * modification to factorial(n), therefore it is also decidable.
+     */
+    return factToFind * factorial(factToFind - 1);
+}
+
 int main(int argc, string argv[])
 {
     int factToFind = argv[1];
