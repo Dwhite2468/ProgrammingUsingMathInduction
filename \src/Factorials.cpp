5eb5a91f9635a970ff6f47d262b1a40fd2ed46eb
Dwhite2468 <urkman2468@gmail.com>
2024-12-02 20:04:37 -0500

    Create Factorials.cpp

---
+++ \src/Factorials.cpp
@@ -0,0 +1,11 @@
+#include<string>
+
+using namespace std;
+
+int main(int argc, string argv[])
+{
+    int factToFind = argv[1];
+    cout << factorial(factToFind);
+    cout << endl;
+    return 0;
+}
