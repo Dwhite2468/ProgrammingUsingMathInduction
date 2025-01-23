f5fa4bdd4b822846950d79b0c6d991bd2bd960d0
Dwhite2468 <urkman2468@gmail.com>
2025-01-23 17:55:49 -0500

    Update binarySearchTree.cpp

--- \src/binarySearchTree.cpp
+++ \src/binarySearchTree.cpp
@@ -4,12 +4,25 @@ BinarySearchTree::find(int toFind)
 {
   int data = this->data;
   int toReturn = -1;
+  /**
+   * Base Case:  We have already reached the value that we are searching for,
+   * so just return the value.
+   */
   if(data == toFind)
   {
     toReturn = data;
   }
+  /**
+   * Inductive Assumption: The data in the left-hand side of the tree
+   * can be arranged into a binary search tree with O(log(n)) retrieval.
+   */
   if(toFind < data)
   {
+    /**
+     * Inductive Step: A value larger than every one in the left-hand side
+     * is now placed at the head of the tree.   The resulting new tree has
+     * of N+1 and is also a binary search tree.  QED
+     */
     toReturn = this->left::find(toFind);
   }
   if(toFind > data)
